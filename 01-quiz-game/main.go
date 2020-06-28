49b6cd0b52dc159fe368dd13734e1a1404934490
Adam Siekierski <adam.siekiera@outlook.com>
2020-06-28 10:57:07 +0200

    Base of quiz

--- 01-quiz-game/main.go
+++ 01-quiz-game/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	csvFilename := flag.String("csv", "questions.csv", "a csv file name in format of question,answer")
+	csvFilename := flag.String("csv", "problems.csv", "a csv file name in format of question,answer")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
@@ -18,9 +18,41 @@ func main() {
 	}
 
 	r := csv.NewReader(file)
-	questions, _ := r.ReadAll()
+	lines, err := r.ReadAll()
 
-	fmt.Println(questions)
+	if err != nil {
+		exit(fmt.Sprintf("failed to parse the csv file: %s", *csvFilename), 1)
+	}
+
+	problems := parseLines(lines)
+
+	correct := 0
+
+	for i, p := range problems {
+		fmt.Printf("problem #%d: %s = ", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("correct answers: %d out of %d\n", correct, len(problems))
+}
+
+func parseLines(problems [][]string) []Problem {
+	ret := make([]Problem, len(problems))
+
+	for i, problem := range problems {
+		ret[i] = Problem{q: problem[0], a: problem[1]}
+	}
+
+	return ret
+}
+
+type Problem struct {
+	q string
+	a string
 }
 
 func exit(msg string, code int) {
